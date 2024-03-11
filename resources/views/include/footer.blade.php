39d9d78195a06c6f2e46e4082e6a7e31efa499b9
Adelia <adeliasalsabila1001@gmail.com>
2024-03-11 22:06:11 +0700

    footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,13 +1,56 @@
-{{-- footer --}}
-<footer class="footer-no-gap">
-    <div class="container">
-      <div class="col-lg-12">
-        <p>
-          Copyright © 2048 Villa Agency Co., Ltd. All rights reserved. Design:
-          <a rel="nofollow" href="https://templatemo.com" target="_blank"
-            >TemplateMo</a
-          >
-        </p>
-      </div>
+<footer class="footer-distributed">
+    
+    <div class="footer-left">
+
+      <h3>Solo<span>Smartcity</span></h3>
+
+      <p class="footer-links">
+        <a href="#" class="link-1">Beranda</a>
+        
+        <a href="#">Tentang</a>
+      
+        <a href="#">Dimensi</a>
+      
+        <a href="#">Solo Event</a>
+        
+        <a href="#">Digital Experience</a>
+      </p>
+
+      <p class="footer-company-name">Company Name © 2015</p>
     </div>
-  </footer>
+
+    <div class="footer-center">
+
+      <div>
+        <i class="fa fa-map-marker"></i>
+        <p><span>Jl. Jend. Sudirman No.2, Kp. Baru</span> Kec. Ps. Kliwon, Kota Surakarta, Jawa Tengah 57133</p>
+      </div>
+
+      <div>
+        <i class="fa fa-phone"></i>
+        <p>(0271) 644315</p>
+      </div>
+
+      <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="mailto:support@company.com">support@company.com</a></p>
+      </div>
+
+    </div>
+
+    <div class="footer-right">
+
+      <!-- <p class="footer-company-about">
+        <span>About the company</span>
+        Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+      </p> -->
+
+      <div class="footer-icons">
+
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="#"><i class="fa fa-youtube"></i></a>
+      </div>
+
+    </div>
+
+</footer>
