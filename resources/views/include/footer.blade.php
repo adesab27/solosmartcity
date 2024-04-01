27ea51e6dce90385b441e4bf162573335318f644
Adelia <adeliasalsabila1001@gmail.com>
2024-04-01 13:01:04 +0700

    footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -6,7 +6,7 @@
 
       <p class="footer-company-name">	&copy; Solo SmartCity</p>
 
-      <p class="footer-company-name">
+      <p class="footer-company-name desc">
         Smart city atau kota cerdas adalah kota yang mengintegrasikan teknologi informasi dan komunikasi dalam tata kelola sehari-hari. Tujuannya adalah untuk meningkatkan efisiensi, memperbaiki pelayanan publik, dan meningkatkan kesejahteraan warga.
       </p>
     </div>
@@ -14,17 +14,17 @@
     <div class="footer-center">
 
       <div>
-        <i class="fa fa-map-marker"></i>
+        <a href="#"><img src="{{asset("assets/images/map.png")}}"></a>
         <p><span>Jl. Jend. Sudirman No.2, Kp. Baru</span> Kec. Ps. Kliwon, Kota Surakarta, Jawa Tengah 57133</p>
       </div>
 
       <div>
-        <i class="fa fa-phone"></i>
+        <a href="#"><img src="{{asset("assets/images/phone.png")}}"></a>
         <p>(0271) 644315</p>
       </div>
 
       <div>
-        <i class="fa fa-envelope"></i>
+        <a href="#"><img src="{{asset("assets/images/email.png")}}"></a>
         <p><a href="mailto:support@company.com">support@company.com</a></p>
       </div>
 
@@ -32,9 +32,9 @@
 
     <div class="footer-right">
       <div class="footer-icons">
-        <a href="#"><i class="bi bi-youtube"></i></a>
-        <a href="#"><i class="bi bi-instagram"></i></a>
-        <a href="#"><i class="bi bi-tiktok"></i></a>
+        <a href="#"><img src="{{asset("assets/images/instagram.png")}}"></a>
+        <a href="#"><img src="{{asset("assets/images/tiktok.png")}}"></a>
+        <a href="#"><img src="{{asset("assets/images/youtube.png")}}"></i></a>
       </div>
       <!-- <p class="footer-company-about">
         <span>About the company</span>
--- resources/views/include/style.blade.php
+++ resources/views/include/style.blade.php
@@ -5,6 +5,8 @@
 <!-- Include Font Awesome CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <!-- Additional CSS Files -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" crossorigin=""/>
 <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
